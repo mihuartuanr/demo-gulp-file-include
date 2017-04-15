c9c05d31730d6ca5f6b40269ed71678b602d2840
zhangchao <zhangchaouidesign@163.com>
2017-04-15 16:49:22 +0800

    include data

--- demo02/config/gulpfile.js
+++ demo02/config/gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var fileInclude = require('gulp-file-include');
 
-gulp.task('default',function(){
-    //src和dest的路径都是以gulpfile.js为基寻找的
-    gulp.src('../src/client/*.html')
+gulp.task('default', function () {
+  //src和dest的路径都是以gulpfile.js为基寻找的
+  gulp.src('../src/client/*.html')
     .pipe(fileInclude({
-        prefix:'@@',
-        basepath:'@file'
+      prefix: '@@',
+      basepath: '@file',
+      context: {
+        name: '这是全局的变量'
+      }
     }))
     .pipe(gulp.dest('../dist'))
 })
